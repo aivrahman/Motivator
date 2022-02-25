27ec92a69dc4cf58aa6cebf141c30beccca35a07
Amer Rahman <aiv.rahman@gmail.com>
2022-02-25 11:09:41 -0600

    Adding comments to clarify code

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+//The array of different values that will be included in Hex color code
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+//Words of motivation for the user
 const words = ['WHO\'S GONNA CARRY THE BOATS? ', 'YESTERDAY, YOU SAID TOMORROW! ', 'STOP WHEN YOU\'RE DONE! ', 'Stay HARD! ', 'Start NOW! ', 'TAKE IT! ', 'Follow your dreams '];
 const btn = document.getElementById("btn");
 const motivate = document.querySelector(".words"); 
 
+//Functionality of clicking the button where random values are generated for phrases and hex code (5 values)
 btn.addEventListener("click", function () {
     let hexColor = "#";
     let motivateMe = "";
@@ -15,11 +19,11 @@ btn.addEventListener("click", function () {
     motivate.textContent = motivateMe
 });
 
-
+//Generate random number
 function getRandomNumber(){
     return Math.floor(Math.random() * hex.length);
 }
-
+//Generate random phrases
 function getRandomWords(){
     return Math.floor(Math.random() * words.length); 
 }
